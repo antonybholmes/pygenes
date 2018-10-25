8ce2241f57b1255a12fb1c407df942b1a9ae80b9
Antony Holmes <antony.b.holmes@gmail.com>
2018-10-25 15:47:55 -0400

    Updates and bug fixes.

--- api/views.py
+++ api/views.py
@@ -42,15 +42,16 @@ def find(request):
     # Defaults should find BCL6
     id_map = libhttp.parse_params(request, {'genome':'Human', 'track':'gencode', 'assembly':'grch38', 'chr':'chr3', 's':187721377, 'e':187736497, 't':'g'})
     
-    track = id_map['track'][0]
-    assembly = id_map['assembly'][0]
+    genome = id_map['genome'][0].lower()
+    assembly = id_map['assembly'][0].lower()
+    track = id_map['track'][0].lower()
     
     loc = libhttpdna.get_loc_from_params(id_map)
     
     if loc is None:
         return JsonResponse({}, safe=False)
     
-    dir = os.path.join(settings.DATA_DIR, track, assembly)
+    dir = os.path.join(settings.DATA_DIR, genome, assembly, track)
     
     reader = libgfb.GFBReader(track, assembly, dir)
     
@@ -68,11 +69,12 @@ def search(request):
     
     id_map = libhttp.parse_params(request, {'genome':'Human', 'track':'gencode', 'assembly':'grch38', 's':'BCL6', 't':'g'})
     
-    track = id_map['track'][0]
-    assembly = id_map['assembly'][0]
+    genome = id_map['genome'][0].lower()
+    assembly = id_map['assembly'][0].lower()
+    track = id_map['track'][0].lower()
     search = id_map['s'][0]
     
-    dir = os.path.join(settings.DATA_DIR, track, assembly)
+    dir = os.path.join(settings.DATA_DIR, genome, assembly, track)
     
     reader = libgfb.GFBReader(track, assembly, dir)
     
@@ -96,7 +98,7 @@ def databases(request):
         d = os.path.join(settings.DATA_DIR, file)
         
         if os.path.isdir(d):
-            track = file
+            genome = file
             
             files2 = os.listdir(d)
             
@@ -106,6 +108,14 @@ def databases(request):
                 if os.path.isdir(d2):
                     assembly = file2
                     
-                    ret.append({'track':track, 'assembly':assembly})
+                    files3 = os.listdir(d2)
+            
+                    for file3 in files3:
+                        d3 = os.path.join(d2, file3)
+                
+                        if os.path.isdir(d3):
+                            track = file2
+                    
+                            ret.append({'genome':genome.capitalize(), 'assembly':assembly, 'track':track})
     
     return JsonResponse(ret, safe=False)
