ff1dfbe9448436c98e6f8c0d030e977b647df995
Antony Holmes <antony.b.holmes@gmail.com>
2019-06-03 19:31:24 -0400

    Updates and bug fixes.

--- genes/settings.py
+++ genes/settings.py
@@ -107,7 +107,7 @@ CACHES = {
         'LOCATION': 'redis://52.20.36.165:6379/2',
 	'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            'PASSWORD': 'ahXob9au3hu3fi4su4ci3HouRivu9chi',
+            'PASSWORD': '**** REMOVED ****',
         }
     }
 }
