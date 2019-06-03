6b077127ec0a8718354ba9c6cc3fcee729001c02
Antony Holmes <antony.b.holmes@gmail.com>
2019-06-03 12:17:03 -0400

    Updates and bug fixes.

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.core.cache import cache
 import os
 
 from genes import settings
@@ -12,7 +13,7 @@ import libgfb
 import libgenomic
 
 def _gene_to_json(gene):
-    return {'loc':gene.loc, 'strand':gene.strand, 'type':gene.level, 'ids':gene.annotations, 'tags':gene.tags}
+    return {'loc':gene.loc, 'strand':gene.strand, 'type':gene.level, 'ids':gene.properties, 'tags':gene.tags}
 
 def _genes_to_json(genes):
     ret = []
@@ -32,7 +33,7 @@ def _genes_to_json(genes):
     return ret
 
 def about(request):
-    return JsonResponse({'name':'genes','version':'1.0','copyright':'Copyright (C) 2018 Antony Holmes'}, safe=False)
+    return JsonResponse({'name':'genes','version':'2.0','copyright':'Copyright (C) 2018-2019 Antony Holmes'}, safe=False)
 
 def find(request):
     """
@@ -40,11 +41,35 @@ def find(request):
     """
     
     # Defaults should find BCL6
-    id_map = libhttp.parse_params(request, {'genome':'Human', 'track':'gencode', 'assembly':'grch38', 'chr':'chr3', 's':187721377, 'e':187736497, 't':'g'})
+    #id_map = libhttp.parse_params(request, {'genome':'Human', 'track':'gencode', 'assembly':'grch38', 'chr':'chr3', 's':187721377, 'e':187736497, 't':'g'})
     
-    genome = id_map['genome'][0].lower()
-    assembly = id_map['assembly'][0].lower()
-    track = id_map['track'][0].lower()
+    
+    id_map = libhttp.ArgParser() \
+        .add('genome', default_value='Human') \
+        .add('track', default_value='gencode') \
+        .add('assembly', default_value='grch38') \
+        .add('chr', default_value='chr3') \
+        .add('s', default_value=187721377) \
+        .add('e', default_value=187736497) \
+        .parse(request)
+    
+    
+    genome = id_map['genome'].lower()
+    assembly = id_map['assembly'].lower()
+    track = id_map['track'].lower()
+    
+    # try to stop users specifying wrong genome
+    if 'mm' in assembly:
+        genome = 'mouse'
+        
+    cache_key = '_'.join(['gene_search', genome, assembly, track, search, str(s), str(e)])
+        
+    data = cache.get(cache_key)
+    
+    # shortcut and return cached copy
+    if data is not None:
+        #print('Using gene search cache of', cache_key)
+        return data
     
     loc = libhttpdna.get_loc_from_params(id_map)
     
@@ -60,7 +85,11 @@ def find(request):
     
     gl = _genes_to_json(genes)
     
-    return JsonResponse({'loc':loc.__str__(), 'genes':gl}, safe=False)
+    data = JsonResponse({'loc':loc.__str__(), 'genes':gl}, safe=False)
+    
+    cache.set(cache_key, data, settings.CACHE_TIME_S)
+        
+    return data
 
 
 def search(request):
@@ -68,12 +97,33 @@ def search(request):
     Search for genes by name.
     """
     
-    id_map = libhttp.parse_params(request, {'genome':'Human', 'track':'gencode', 'assembly':'grch38', 's':'BCL6', 't':'g'})
+    #id_map = libhttp.parse_params(request, {'genome':'Human', 'track':'gencode', 'assembly':'grch38', 's':'BCL6', 't':'g'})
     
-    genome = id_map['genome'][0].lower()
-    assembly = id_map['assembly'][0].lower()
-    track = id_map['track'][0].lower()
-    search = id_map['s'][0]
+    id_map = libhttp.ArgParser() \
+        .add('genome', default_value='Human') \
+        .add('track', default_value='gencode') \
+        .add('assembly', default_value='grch38') \
+        .add('chr', default_value='chr3') \
+        .add('s', default_value='BCL6') \
+        .add('e', default_value=187736497) \
+        .parse(request)
+    
+    genome = id_map['genome'].lower()
+    assembly = id_map['assembly'].lower()
+    track = id_map['track'].lower()
+    search = id_map['s']
+    
+    if 'mm' in assembly:
+        genome = 'mouse'
+        
+    cache_key = '_'.join(['gene_search', genome, assembly, track, search])
+        
+    data = cache.get(cache_key)
+    
+    # shortcut and return cached copy
+    if data is not None:
+        #print('Using gene search cache of', cache_key)
+        return data
     
     dir = os.path.join(settings.DATA_DIR, genome, assembly, track)
     
@@ -83,13 +133,25 @@ def search(request):
     
     gl = _genes_to_json(genes)
     
-    return JsonResponse(gl, safe=False)
+    data = JsonResponse(gl, safe=False)
+    
+    cache.set(cache_key, data, settings.CACHE_TIME_S)
+        
+    return data
 
 
 def databases(request):
     """
     List available databases.
     """
+    
+    data = cache.get('databases') # returns None if no key-value pair
+    
+    # shortcut and return cached copy
+    if data is not None:
+        #print('Using vfs cache of', cache_key)
+        return data
+        
     
     files = os.listdir(settings.DATA_DIR)
     
@@ -119,4 +181,10 @@ def databases(request):
                     
                             ret.append({'genome':genome.capitalize(), 'assembly':assembly, 'track':track})
     
-    return JsonResponse(ret, safe=False)
+    data = JsonResponse(ret, safe=False)
+    
+    cache.set('databases', data, settings.CACHE_TIME_S)
+        
+    return data
+    
+    
